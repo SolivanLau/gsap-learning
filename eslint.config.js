c946172f00230d3f0763f0e67a531566a6c5896b
solivan-lau <solivan_lau@outlook.com>
2024-11-02 18:46:07 -0400

    CONFIG: eslint configuration with prettier, typescript, and astro plugins.

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+    { ignores: ["node_modules/** , .dist/**"] },
+    {
+        files: ["**/*.{js,jsx,mjs,cjs,ts,tsx,astro}"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
+    // js
+    js.configs.recommended,
+    // typescript
+    ...tseslint.configs.recommended,
+    // astro
+    ...eslintPluginAstro.configs.recommended,
+    ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+    // Override for astro env.d.ts
+    {
+        files: ["src/env.d.ts"],
+        rules: {
+            "@typescript-eslint/triple-slash-reference": "off",
+        },
+    },
+    // Override for prettier: prettier formats code, eslint fixes code
+    eslintConfigPrettier,
+];
